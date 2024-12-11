618120fe6771b915389f074fbda4df4440779511
Elhassan <chakouki.el.hassan@gmail.com>
2024-12-11 13:41:28 +0100

    modify ApiKey, by giving a guide to know how you can git your own apikey if you want to work with this code

--- Javascript/Project1/script.js
+++ Javascript/Project1/script.js
@@ -1,4 +1,4 @@
-const apikey= 'Elhassan7-ApiKey';
+const apikey= 'Elhassan7-ApiKey'; //you need to sign up in https://newsapi.org and get your own api-key
 
 const blogContainer= document.getElementById('blog-container');
 const searchField= document.getElementById('search-input');

b0f8499486b2004bc98367341f3dd7d970580717
Elhassan <chakouki.el.hassan@gmail.com>
2024-12-20 12:16:11 +0100

    update

--- Javascript/Project2/js/script.js
+++ Javascript/Project2/js/script.js
@@ -3,14 +3,14 @@ import Student from "./student.js";
 const displayStudent = function(){
     return Student.allStudents().then(response => {
         const tableBody = response.map(student => {
-            const {id, name, level, note} = student;
+            const {id, name, date, note} = student;
 
             return `<tr> 
                 <td>${id}</td>
                 <td>${name}</td>
-                <td>${level}</td>
-                <td>${note}</td>
-                <td></td>                
+                <td>${date}</td>
+                <td><span class="badge rounded-pill ${note<10?'text-bg-danger':'text-bg-success'}">${note} /${Student.MAX_NOTE}</span></td>
+                <td><button class='btn btn-danger btn-sm delete' onClick='deleteStudent(${id})''>Remove</button></td>                
             </tr>`
         })
         return tableBody
@@ -19,11 +19,36 @@ const displayStudent = function(){
 
 const renderStudents= function(){
     const body= document.querySelector('.list-students');
-    displayStudent().then(data =>  body.innerHTML = data)
+    displayStudent().then(data => 
+        body.innerHTML = data.join(' ')
+    ); 
+    init();
 };
 
-renderStudents();
-console.log(displayStudent().then(data => console.log(data)))
+const addStudent= (event) =>{
+    event.preventDefault()
+    const [name, date, note]=document.querySelectorAll('#name, #date, #note')
+    const student = new Student(name.value, date.value, note.value)
+    student.addStudent()
+}
 
+window.deleteStudent = (id) =>{
+    Student.deleteStudent(id)
+}
+
+const init = function(){
+    document.querySelector('#refresh').addEventListener('click', () =>{
+        renderStudents();
+    })
+    document.querySelector('#add').addEventListener('click', (event) =>{
+        addStudent(event);
+    })
+    document.querySelector('.delete').addEventListener('click', (event) =>{
+        deleteStudent(event);
+    })
+    
+}
+
+renderStudents();
 
 
--- Javascript/Project2/js/student.js
+++ Javascript/Project2/js/student.js
@@ -1,9 +1,10 @@
 import { ENDPOINT } from "./constants.js";
 
 export default class Student {
-    constructor(name, age, note){
+    static MAX_NOTE=20;
+    constructor(name, date, note){
         this.name=name;
-        this.age=age;
+        this.date=date;
         this.note=note;
     }
 
@@ -11,5 +12,27 @@ export default class Student {
         const  response = await fetch(ENDPOINT);
         const students = await response.json();
         return students
+    }
+
+    addStudent = async function(){
+        const  response = await fetch(ENDPOINT,{
+            method: 'POST',
+            body: JSON.stringify({
+                name: this.name,
+                date: this.date,
+                note: this.note
+            })
+        });
+        return response
+    }
+
+    static deleteStudent= async function (id) {
+        const  response = await fetch(ENDPOINT+'/?id='+id,{
+            method: 'DELETE',
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        return response
     }
 }

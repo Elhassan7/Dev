0e7a6a375f4c21f33a8da2d274f48a0ff364372c
Elhassan <chakouki.el.hassan@gmail.com>
2025-01-09 22:39:59 +0100

    update

--- React/project3-gemini_App/src/App.jsx
+++ React/project3-gemini_App/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Sidebar from './componenets/Sidebar/Sidebar'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <>
+            <Sidebar />
+        </>
+    )
 }
 
 export default App

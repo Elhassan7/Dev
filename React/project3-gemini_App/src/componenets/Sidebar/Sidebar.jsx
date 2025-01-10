004234c226f9f1ca2c8bcd033bc1f1668a386fad
Elhassan <chakouki.el.hassan@gmail.com>
2025-01-10 22:31:10 +0100

    Final touch for Gemini App

--- React/project3-gemini_App/src/componenets/Sidebar/Sidebar.jsx
+++ React/project3-gemini_App/src/componenets/Sidebar/Sidebar.jsx
@@ -1,26 +1,38 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context';
 
 const Sidebar = () => {
 
     const [extended, setExtended] = useState(false);
+    const { onSent, prevPrompt, setRecentPrompt, newCaht } = useContext(Context)
+
+    const loadPrompt = async (prompt) => {
+        setRecentPrompt(prompt)
+        await onSent(prompt)
+    }
 
     return (
         <div className='sidebar'>
             <div className="top">
-                <img onClick={ () => setExtended( prev => !prev)} className='menu' src={assets.menu_icon} alt="menu icon" />
-                <div className="new-chat">
+                <img onClick={() => setExtended(prev => !prev)} className='menu' src={assets.menu_icon} alt="menu icon" />
+                <div onClick={()=> newCaht()} className="new-chat">
                     <img src={assets.plus_icon} alt="New chat icon" />
                     {extended ? <p>Add New Chat</p> : null}
                 </div>
                 {extended ?
                     <div className="recent">
                         <p className='recent-title'>Recent</p>
-                        <div className="recent-entry">
-                            <img src={assets.message_icon} alt="message icon" />
-                            <p>what is react...</p>
-                        </div>
+                        {prevPrompt.map((item, index) => {
+                            return (
+                                <div onClick={()=>loadPrompt(item)} key={index} className="recent-entry">
+                                    <img src={assets.message_icon} alt="message icon" />
+                                    <p>{item.length<15?item:item.slice(0,15)+' ...'}</p>
+                                </div>
+                            )
+                        })}
+
                     </div> : null
                 }
 
